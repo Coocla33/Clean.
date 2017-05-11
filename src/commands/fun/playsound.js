6c7ac2b3aec8b399f1cfd1c73a1344c0ce0ad2f0
Coocla33 <Coocla33@gmail.com>
2017-05-11 15:58:34 +0200

    Updated PlaySound command.
    Updated PlaySound command.
    Fully redid it, made it more stable. No crasherino's again i hope.

--- src/commands/fun/playsound.js
+++ src/commands/fun/playsound.js
@@ -3,17 +3,17 @@ exports.data = {
   'aliases': ['ps'],
   'desc': 'Play a sound inside your voice channel!',
   'usage': 'playsound [sound]',
-  'note': 'You need to be inside a Voice Channel to use this command!\nSound list: `Airhorn`, `MonsterKill`, `Troll`, `Applause`, `Nigger`, `Scream`, `TheFuckYouDo`, `MegaFaggot`',
+  'note': 'You need to be inside a Voice Channel to use this command!\n**Sound list:** `Airhorn`, `MonsterKill`, `Troll`, `Applause`, `Nigger`, `Scream`, `TheFuckYouDo`, `MegaFaggot`',
   'indev': true
 }
 
 exports.run = function(msg, data) {
   return new Promise((resolve, reject) => {
     if (msg.member.voiceChannel) {
-      msg.member.voiceChannel.join().then((connection) => {
-        if (connection.speaking) {
-          msg.channel.send('I am already playing a sound in this channel!')
-        } else {
+      if (!msg.guild.voiceConnection) {
+        //Not in a voiceChannel inside this guild.
+        msg.member.voiceChannel.join().then((connection) => {
+          //Setup sound to play
           let files = ['airhorn.mp3', 'monsterkill.mp3', 'troll.mp3', 'applause.mp3', 'nigger.mp3', 'scream.mp3', 'thefuckyoudo.mp3', 'megafaggot.mp3']
           let final = files[Math.floor(Math.random() * files.length)]
           if (data.suffix) {
@@ -21,16 +21,29 @@ exports.run = function(msg, data) {
               final = data.suffix.toLowerCase() + '.mp3'
             }
           }
-          msg.channel.send(final)
+          //Play Sound
           const dispatcher = connection.playFile(srcDirectory + '/sounds/' + final)
-          msg.channel.send('Sound Played!')
-          dispatcher.on('end', () => {
-            connection.disconnect()
+          msg.channel.send('Playing Sound!').then((Msg) => {
+            dispatcher.on('end', () => {
+              //When sound has been played, disconnect from voiceChannel and remove the 'Sound Played' message.
+              connection.disconnect()
+              Msg.delete()
+            })
           })
+        })
+      } else {
+        //In a voiceChannel inside this guild.
+        if (msg.guild.voiceConnection.channel.id == msg.member.voiceChannel.id) {
+          //Inside same voiceChannel as user.
+          resolve('Bot Already Playing A Sound')
+        } else {
+          //Not in the same voiceChannel as user.
+          resolve('Bot Already In VoiceChannel')
         }
-      })
+      }
     } else {
-      reject('Not In A Voice Channel')
+      //User is not in a voiceChannel.
+      resolve('User Not In VoiceChannel')
     }
   })
 }
